28b3aefd5f5016870025e0cfbf6958f22c50f0f0
dinesh-tech-757 <dineshmoorthi757@gmail.com>
2025-04-01 13:58:53 +0530

    dotenv config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import userModel from "./models/UserModel.js";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 
